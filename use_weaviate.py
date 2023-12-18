c2f4460eb5f7b989d5fbcb585b2e4006a1bea26b
Jason Sun <sunapi386@gmail.com>
2023-12-18 11:29:03 -0500

    Add MD5 hash checking functions for weaviate

--- use_weaviate.py
+++ use_weaviate.py
@@ -1,3 +1,4 @@
+import time
 import weaviate
 
 
@@ -38,6 +39,32 @@ class WeaviateEmbeddingStore:
         )
 
 
+    def check_md5_exists(self, md5_hash, model_name):
+        start_time = time.time()
+        query = f"""
+        {{
+            Get {{
+                FileEmbedding(where: {{operator: And, operands: [{{path: ["md5Hash"], operator: Equal, valueString: "{md5_hash}"}}, {{path: ["modelName"], operator: Equal, valueString: "{model_name}"}}]}}) {{
+                    md5Hash
+                }}
+            }}
+        }}
+        """
+        result = self.client.query.raw(query)
+        duration = time.time() - start_time
+        exists = bool(result['data']['Get']['FileEmbedding'])
+        return exists, duration
+
+    def check_multiple_md5_exists(self, md5_hashes, model_name):
+        start_time = time.time()
+        results = []
+        for md5_hash in md5_hashes:
+            exists, _ = self.check_md5_exists(md5_hash, model_name)
+            results.append((md5_hash, exists))
+        duration = time.time() - start_time
+        return results, duration
+
+
 # Usage Example
 weaviate_url = "http://henderson:8080"
 print("Connecting to Weaviate at", weaviate_url)
@@ -74,6 +101,19 @@ query = """
 
 raw_all = store.client.query.raw(query)
 print("raw_all", raw_all)
+
+
+# Check a single MD5 hash
+exists, query_time = store.check_md5_exists("abc123", "example_model")
+print(f"Exists: {exists}, Query Time: {query_time} seconds")
+
+# Check multiple MD5 hashes
+md5_hashes = ["abc123", "def456", "ghi789"]
+results, total_query_time = store.check_multiple_md5_exists(md5_hashes, "example_model")
+for md5_hash, exists in results:
+    print(f"MD5: {md5_hash}, Exists: {exists}")
+print(f"Total Query Time for Multiple MD5s: {total_query_time} seconds")
+
 
 # Delete
 store.delete_embedding(md5_hash)
