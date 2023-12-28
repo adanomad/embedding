71c64227e17ef9bd642d1d7e5d1fcee98eaee42d
Jason Sun <sunapi386@gmail.com>
2023-12-27 19:17:30 -0500

    Refactor EmbeddingSearch component to use weaviateClient and improve search functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { Container, Paper } from "@mantine/core";
-import FileEmbeddingCount from "./FileEmbeddingCount";
 import EmbeddingSearch from "./EmbeddingSearch";
 
 const App: React.FC = () => {
   return (
     <Container size="md" className="App">
       <Paper>
-        <FileEmbeddingCount />
         <EmbeddingSearch />
       </Paper>
     </Container>
--- frontend/src/EmbeddingSearch.tsx
+++ frontend/src/EmbeddingSearch.tsx
@@ -1,7 +1,6 @@
 // src/EmbeddingSearch.jsx
 import { useState } from "react";
 import axios from "axios";
-import { useLazyQuery } from "@apollo/client";
 import {
   Anchor,
   Text,
@@ -11,57 +10,75 @@ import {
   Image,
   Pagination,
 } from "@mantine/core";
-import { gql } from "@apollo/client";
 import { usePagination } from "@mantine/hooks";
+import { client } from "./weaviateClient";
 
-const GET_CLOSEST_FILE_EMBEDDING = gql`
-  query GetClosestFileEmbedding($embedding: [Float!]!) {
-    Get {
-      FileEmbedding(
-        nearVector: { vector: $embedding, distance: 0.80 }
-        limit: 3
-      ) {
-        fileName
-        _additional {
-          distance
-        }
-      }
-    }
-  }
-`;
+interface SearchResult {
+  fileName: string;
+  _additional: { distance: number };
+}
 
 const EmbeddingSearch = () => {
   const [inputText, setInputText] = useState("");
-  const [embedding, setEmbedding] = useState(null);
-  const [getClosestFileEmbedding, { data, loading, error }] = useLazyQuery(
-    GET_CLOSEST_FILE_EMBEDDING
-  );
-  const [page, onChange] = useState(1);
-  const pagination = usePagination({ total: 10, page, onChange });
+  const [embedding, setEmbedding] = useState<number[]>([]);
+  const [searchResults, setSearchResults] = useState<SearchResult[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const [activePage, setActivePage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  // setTotalPages doesn't work because we don't know the total number of results - need estimate
+  const limit = 5;
+
+  const fetchSearchResults = async (embedding: number[], page: number) => {
+    try {
+      const offset = (page - 1) * limit;
+      const results = await client.graphql
+        .get()
+        .withClassName("FileEmbedding")
+        .withNearVector({ vector: embedding }) //distance: 0.68
+        .withLimit(limit)
+        .withOffset(offset)
+        .withFields("fileName _additional { distance }")
+        // .withAutocut(1)
+        .do();
+
+      // Reversing the order of search results
+      const reversedResults = results.data.Get.FileEmbedding.slice().reverse();
+      setSearchResults(reversedResults);
+
+      // Calculate total pages if you have that information
+      // setTotalPages(calculatedTotalPages);
+    } catch (err) {
+      console.error("Error fetching embedding:", err);
+      setError(err instanceof Error ? err.message : String(err));
+    }
+  };
 
   const handleSearch = async () => {
+    setLoading(true);
     try {
-      const response = await axios.post("http://glassbox.ds:5000/embed", {
+      const embedResponse = await axios.post("http://glassbox.ds:5000/embed", {
         text: inputText,
       });
-      setEmbedding(response.data.embedding);
-      getClosestFileEmbedding({
-        variables: { embedding: response.data.embedding, limit: 10, offset: 1 },
-      });
-    } catch (error) {
-      console.error("Error fetching embedding:", error);
+      setEmbedding(embedResponse.data.embedding);
+      await fetchSearchResults(embedResponse.data.embedding, 1);
+    } catch (err) {
+      console.error("Error fetching embedding:", err);
+      setError(err instanceof Error ? err.message : String(err));
     }
+    setLoading(false);
   };
 
   const handlePageChange = (page: number) => {
-    pagination.setPage(page);
-    const offset = (page - 1) * 5;
-    if (embedding) {
-      getClosestFileEmbedding({ variables: { embedding, offset } });
+    setActivePage(page);
+    if (embedding.length > 0) {
+      fetchSearchResults(embedding, page);
+    } else {
+      console.error("No embedding found");
     }
   };
 
-  const flaskEndpoint = "http://glassbox.ds:5000/image?fileName="; // Update with your Flask app's URL
+  const flaskEndpoint = "http://glassbox.ds:5000/image?fileName=";
 
   return (
     <Card shadow="sm" padding="lg">
@@ -79,9 +96,9 @@ const EmbeddingSearch = () => {
 
       {loading && <p>Loading...</p>}
       {error && <p>Error: {error.message}</p>}
-      {data && (
+      {searchResults && (
         <Card>
-          {data.Get.FileEmbedding.map(
+          {searchResults.map(
             (
               item: { fileName: string; _additional: { distance: number } },
               index: number
@@ -90,7 +107,7 @@ const EmbeddingSearch = () => {
                 <Anchor href={`${flaskEndpoint}${item.fileName}`}>
                   {index} File Name: {item.fileName}
                 </Anchor>
-                <Text>Distance: {item._additional.distance.toFixed(2)}</Text>
+                <Text>Distance: {item._additional.distance.toFixed(4)}</Text>
                 <Image
                   src={`${flaskEndpoint}${item.fileName}`}
                   alt={item.fileName}
@@ -100,7 +117,11 @@ const EmbeddingSearch = () => {
               </Card>
             )
           )}
-          <Pagination total={10} onChange={handlePageChange} />
+          <Pagination
+            value={activePage}
+            total={totalPages}
+            onChange={handlePageChange}
+          />
         </Card>
       )}
     </Card>
